198848045f9e47c0ea0b12d49f61c0bd68d24f14
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-12-24 19:04:05 +0300

    HW #70 Drone-med

---
+++ src/main/java/telran/drones/controller/DronesController.java
@@ -0,0 +1,25 @@
+package telran.drones.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import telran.drones.api.*;
+import telran.drones.dto.DroneDto;
+import telran.drones.service.DronesService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+//@AllArgsConstructor
+@RequestMapping(UrlConstants.DRONES)
+@Slf4j
+public class DronesController {
+	final DronesService dronesService;
+    @PostMapping
+    DroneDto registerDrone(@RequestBody @Valid DroneDto droneDto) {
+    	log.debug("received: {}", droneDto);
+    	return dronesService.registerDrone(droneDto);
+    }
+}
